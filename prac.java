deaad64b895a2f8b20c69eec79c7379097eb7095
kuanmol <ramkuma100@gmail.com>
2024-09-25 15:58:42 +0530

    first commit

--- prac.java
+++ prac.java
@@ -1,10 +1,7 @@
+import java.util.Scanner;
+
 public class prac {
-    static int sum(int c, int d){
-        return c+d;
-    }
     public static void main(String[] args) {
-        int a=10;
-        int b=20;
-        System.out.println(sum(a,b));
+
     }
 }
