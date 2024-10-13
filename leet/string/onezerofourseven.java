2b313b9fa06014455ea0e04a421628305e51c431
kuanmol <ramuklomna100@gmail.com>
2024-10-13 11:51:48 +0530

    first commit

---
+++ leet/string/onezerofourseven.java
@@ -0,0 +1,27 @@
+package leet.string;
+
+public class onezerofourseven {
+
+    public static String removeDuplicates(String s) {
+        int end = -1;
+        char[] chars = s.toCharArray();
+
+        for (char c : chars) {
+            if (end >= 0 && chars[end] == c) {
+                end--;
+            } else {
+                chars[++end] = c;
+            }
+        }
+        return new String(chars, 0, end + 1);
+    }
+
+    public static void main(String[] args) {
+
+        String s1 = "abbaca";
+
+        System.out.println(removeDuplicates(s1));
+
+    }
+
+}
