9819d18e1086a1ea4d10164bc60b775954bff194
kuanmol <ramuklomna100@gmail.com>
2024-10-18 20:37:54 +0530

    first commit

--- leet/string/onezerotwoone.java
+++ leet/string/onezerotwoone.java
@@ -2,23 +2,21 @@ package leet.string;
 
 public class onezerotwoone {
     public static void main(String[] args) {
-        String s = "(()())(())";
-        System.out.println(removeOuterParentheses(s));
-
+        String a="(()())(())";
+        System.out.println(removeOuterParentheses(a));
     }
-
     public static String removeOuterParentheses(String s) {
         int len = s.length();
         if (len <= 2) return "";
         char[] c = s.toCharArray();
         StringBuilder newString = new StringBuilder();
         int open = 1;
-        int openLeft = 0;
         for (int i = 1; i < len; i++) {
             if (c[i] == '(') {
                 open++;
                 if (open > 1) newString.append('(');
-            } else {
+            }
+            else {
                 if (open > 1) newString.append(')');
                 open--;
             }
