96bf7dec5330c88cf3047377827a27b870634f72
kuanmol <ramuklomna100@gmail.com>
2024-10-22 21:10:44 +0530

    first commit

--- prac.java
+++ prac.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class prac {
     public static void main(String[] args) {
-
+        for (int i = 0; i >-1 ; i++) {
+            System.out.println("cly");
+        }
     }
 }
