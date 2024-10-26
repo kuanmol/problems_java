9d3a4266a50e795729aa5904a24c31042f001ed1
kuanmol <ramuklomna100@gmail.com>
2024-10-26 12:10:44 +0530

    Your commit message here

---
+++ leet/binarytree/twotwosix.java
@@ -0,0 +1,64 @@
+package leet.binarytree;
+
+public class twotwosix {
+
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(9);
+
+        twotwosix solution = new twotwosix();
+        TreeNode invertedRoot = solution.invertTree(root);
+
+        printPreorder(invertedRoot); // Expected output: 4 7 9 6 2 3 1
+    }
+
+    public static void printPreorder(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        System.out.print(node.val + " ");
+        printPreorder(node.left);
+        printPreorder(node.right);
+    }
+}
+
