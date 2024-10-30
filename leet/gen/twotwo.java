4e4a60d131c8b236782e27eece7a9e816bb504d1
kuanmol <ramuklomna100@gmail.com>
2024-10-30 16:57:27 +0530

    first commit

---
+++ leet/gen/twotwo.java
@@ -0,0 +1,37 @@
+package leet.gen;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class twotwo {
+
+    public static List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        backtrack(result, "", 0, 0, n);
+        return result;
+    }
+
+    private static void backtrack(List<String> result, String current, int open, int close, int max) {
+
+        if (current.length() == max * 2) {
+            result.add(current);
+            return;
+        }
+
+        if (open < max) {
+            backtrack(result, current + "(", open + 1, close, max);
+        }
+
+        if (close < open) {
+            backtrack(result, current + ")", open, close + 1, max);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int n = 3;
+        List<String> combinations = generateParenthesis(n);
+        System.out.println(combinations);
+    }
+}
+
---
+++ leet/string/twozero.java
@@ -0,0 +1,38 @@
+package leet.string;
+
+import java.util.Stack;
+
+public class twozero {
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        char[] openBrackets = {'(', '{', '['};
+        char[] closeBrackets = {')', '}', ']'};
+
+        for (char c : s.toCharArray()) {
+            if (isOpeningBracket(c)) {
+                stack.push(c);
+            } else if (isClosingBracket(c)) {
+                if (stack.isEmpty() || !isMatchingPair(stack.pop(), c)) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+    private static boolean isOpeningBracket(char c) {
+        return c == '(' || c == '{' || c == '[';
+    }
+    private static boolean isClosingBracket(char c) {
+        return c == ')' || c == '}' || c == ']';
+    }
+    private static boolean isMatchingPair(char opening, char closing) {
+        return (opening == '(' && closing == ')') ||
+                (opening == '{' && closing == '}') ||
+                (opening == '[' && closing == ']');
+    }
+
+    public static void main(String[] args) {
+        String s = "{{}";
+        System.out.println(isValid(s));
+    }
+}
