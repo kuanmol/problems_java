51a49e5167905c06e66e5417c9155e8505dbbf62
kuanmol <ramuklomna100@gmail.com>
2024-11-03 19:55:08 +0530

    first commit

--- leetcode/dp/fiveonesix.java
+++ leetcode/dp/fiveonesix.java
@@ -18,7 +18,6 @@ public class fiveonesix {
                 prev = temp;
             }
         }
-
         return dp[n - 1];
     }
     public static void main(String[] args) {
--- prac.java
+++ prac.java
@@ -1,12 +1,6 @@
-import java.util.Scanner;
 
 public class prac {
     public static void main(String[] args) {
-        int a = 0;
-        if(a == 22){
-            System.out.println("dsfgdh");
-        }else if(a == 50){
-            System.out.println("ak");
-        }
+        System.out.println("nitu");
     }
 }
