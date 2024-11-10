340ed04908a5cf2cea8ff18fb7165483938dea87
kuanmol <ramuklomna100@gmail.com>
2024-11-10 23:00:52 +0530

    first commit

---
+++ leetcode/array/threeonetwoseven.java
@@ -0,0 +1,49 @@
+package leetcode.array;
+
+public class threeonetwoseven {
+    public static void main(String[] args) {
+        char[][] grid1 = {
+                {'B', 'W', 'B'},
+                {'B', 'W', 'W'},
+                {'B', 'W', 'B'}
+        };
+        System.out.println(canMakeSquare(grid1)); // Output: true
+
+        char[][] grid2 = {
+                {'B', 'W', 'B'},
+                {'W', 'B', 'W'},
+                {'B', 'W', 'B'}
+        };
+        System.out.println(canMakeSquare(grid2)); // Output: false
+
+        char[][] grid3 = {
+                {'B', 'W', 'B'},
+                {'B', 'W', 'W'},
+                {'B', 'W', 'W'}
+        };
+        System.out.println(canMakeSquare(grid3)); // Output: true
+    }
+
+    public static boolean canMakeSquare(char[][] grid) {
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                int bCount = 0, wCount = 0;
+
+                if (grid[i][j] == 'B') bCount++;
+                else wCount++;
+                if (grid[i][j + 1] == 'B') bCount++;
+                else wCount++;
+                if (grid[i + 1][j] == 'B') bCount++;
+                else wCount++;
+                if (grid[i + 1][j + 1] == 'B') bCount++;
+                else wCount++;
+
+                if (bCount == 4 || wCount == 4 || bCount == 3 || wCount == 3) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
