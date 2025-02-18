4c993569cc165d2abb5c771ea6f8b305cca91844
kuanmol <ramuklomna100@gmail.com>
2025-02-18 22:22:24 +0530

    formatting

--- Main.java
+++ Main.java
@@ -1,10 +1,59 @@
-public class Main {
-    static int a = 2;
 
-    public static void main(String[] args) {
-        int a = 1;
-        int b = 1;
-        System.out.println(a++);
-        System.out.println(++b);
+class Stack {
+    static final int MAX = 1000;
+    int top;
+    int a[] = new int[MAX];
+
+    boolean isEmpty() {
+        return (top < 0);
+    }
+
+    Stack() {
+        top = -1;
+    }
+
+    boolean push(int x) {
+        if (top >= (MAX - 1)) {
+            System.out.println("Stack Overflow");
+            return false;
+        } else {
+            a[++top] = x;
+            System.out.println(x + " pushed into stack");
+            return true;
+        }
+    }
+
+    int pop() {
+        if (top < 0) {
+            System.out.println("Stack Underflow");
+            return 0;
+        } else {
+            int x = a[top--];
+            return x;
+        }
+    }
+
+    int peek() {
+        if (top < 0) {
+            System.out.println("Stack Underflow");
+            return 0;
+        } else {
+            int x = a[top];
+            return x;
+        }
+    }
+
+    void print() {
+        for (int i = top; i > -1; i--) {
+            System.out.print(" " + a[i]);
+        }
     }
 }
+
+class Main {
+    public static void main(String args[]) {
+        Stack s = new Stack();
+        s.peek();
+        s.print();
+    }
+} 
